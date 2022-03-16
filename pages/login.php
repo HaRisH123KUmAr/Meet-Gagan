a0905a846f6398c4ec7b161573a16c39ca263d37
HaRisH123KUmAr <68292739+HaRisH123KUmAr@users.noreply.github.com>
2022-03-16 14:58:04 +0530

    Add files via upload

---
+++ pages/login.php
@@ -0,0 +1,20 @@
+<?php 
+session_start() ;
+$con = mysqli_connect('localhost','root') ; 
+mysqli_select_db($con , 'register') ;  
+
+$email = $_POST['email'] ;
+$pass = $_POST['password'] ;
+
+$s = "select * from db where email = '$email' && password='$pass' " ; 
+
+$result = mysqli_query($con ,$s ) ;
+$num = mysqli_num_rows($result);
+if($num==1) {
+
+header ('location:Welcome.html');;;}
+else
+{ echo  " Please create "  ;}
+
+
+?>
